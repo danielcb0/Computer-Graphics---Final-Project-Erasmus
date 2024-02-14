d6f80a57ed1f210a8a36e010746977b2d45fca1f
Daniel Carrera <83375887+SrDanielCarrera@users.noreply.github.com>
2024-02-14 19:33:13 +0100

    Update position and size Sun, Earth and Moon

--- src/client/SunModule.ts
+++ src/client/SunModule.ts
@@ -6,11 +6,11 @@ export class SunModule {
 
     constructor() {
         // Crear la esfera (Sol)
-        const sphereGeometry = new THREE.SphereGeometry(0.1, 32, 32);
+        const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
         const sphereMaterial = new THREE.MeshStandardMaterial({
             color: 0xffaa00,  // Color de la esfera
             emissive: 0xffaa00,  // Color de la emisión
-            emissiveIntensity: 1
+            emissiveIntensity: 1000
         });
         this.sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
         this.sphere.position.set(0, 3, 0);
